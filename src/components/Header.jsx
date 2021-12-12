4ba92a6e0104ae0ccec511c91a5e023e727328a7
Aseer KT <aseermiqdad@gmail.com>
2021-12-12 21:36:08 +0530

    fix: cart items count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,11 @@ import { useCart } from '../context/cart/CartContext';
 function Header() {
   const { cartItems } = useCart();
 
+  const cartItemsLength = cartItems.reduce(
+    (prev, curr) => (prev += !curr.saved ? curr.qty : 0),
+    0
+  );
+
   return (
     <header className='header'>
       <div className='container'>
@@ -14,7 +19,7 @@ function Header() {
         <nav>
           <Link to='/'>Products</Link>
           <Link to='/cart'>
-            Cart <span className='cart-item-count'>{cartItems.length}</span>
+            Cart <span className='cart-item-count'>{cartItemsLength}</span>
           </Link>
         </nav>
       </div>
